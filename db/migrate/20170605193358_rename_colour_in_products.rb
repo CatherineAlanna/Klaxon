895a68fe5ba9156d4a29fe977ace00af99f04c72
CatherineAlanna <alanna.charnock@gmail.com>
2017-06-05 18:17:16 -0700

    change price datatype and create orders

---
+++ db/migrate/20170605193358_rename_colour_in_products.rb
@@ -0,0 +1,5 @@
+class RenameColourInProducts < ActiveRecord::Migration[5.1]
+  def change
+  	rename_column :products, :colour, :color
+  end
+end
---
+++ db/migrate/20170605194031_change_price_datatype_in_table.rb
@@ -0,0 +1,5 @@
+class ChangePriceDatatypeInTable < ActiveRecord::Migration[5.1]
+  def change
+  	change_column :products, :price, :decimal
+  end
+end
