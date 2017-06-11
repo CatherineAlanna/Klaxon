56fb6f2f91bb11039b49540e11c7f2b160c7a5cd
CatherineAlanna <alanna.charnock@gmail.com>
2017-06-11 16:55:02 -0700

    5.3 contact form creation and email setup

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+	default from: "hello@catherinealanna.com"
+
+	def contact_form(email, name, message)
+		@message = message
+		mail(from: email, 
+			to: 'hello@catherinealanna.com', 
+			subject: "A new contact form message from #{name}")
+	end
+end
