a26a5f78fe6097d2743b341be9643b20db1f0a87
CatherineAlanna <alanna.charnock@gmail.com>
2017-06-13 14:13:48 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612224627) do
-
-  create_table "object_names", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
-ActiveRecord::Schema.define(version: 201_706_060_031_49) do
+ActiveRecord::Schema.define(version: 20170606003149) do
   create_table 'object_names', force: :cascade do |t|
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
@@ -59,5 +53,3 @@ ActiveRecord::Schema.define(version: 201_706_060_031_49) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-end
