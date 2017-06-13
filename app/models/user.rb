62572ca01c57ae7aec4c442d18106a24084807ea
CatherineAlanna <alanna.charnock@gmail.com>
2017-06-13 14:27:55 -0700

    changes to user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true.
 # User
 class User < ActiveRecord::Base
+  has_many :orders
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :orders
 end
