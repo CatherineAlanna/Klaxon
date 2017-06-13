d85216e85d6a298ef8319d12f67c5888c842aa9e
CatherineAlanna <alanna.charnock@gmail.com>
2017-06-13 16:25:34 -0700

    adjust merge conflicts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,11 +62,7 @@ class UsersController < ApplicationController
   end
 
   private
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> master
     # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 class Order < ApplicationRecord
-=======
 # frozen_string_literal: true.
 # Order
-class Order < ActiveRecord::Base
->>>>>>> master
 	belongs_to :product
   belongs_to :user
 end
