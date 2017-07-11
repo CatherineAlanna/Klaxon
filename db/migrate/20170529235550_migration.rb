c273175b13e5ba7deec4b9229901e047239efe7e
CatherineAlanna <alanna.charnock@gmail.com>
2017-07-10 20:24:21 -0700

    updating devise users

--- db/migrate/20170529235550_migration.rb
+++
@@ -1,3 +0,0 @@
-class Migration < ActiveRecord::Migration[5.1]
-  def change; end
-end
--- db/migrate/20170627191403_remove_object_names_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveObjectNamesTable < ActiveRecord::Migration[5.1]
-  def up
-    drop_table :object_names
-  end
-end
